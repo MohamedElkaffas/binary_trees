28a151ad61b86bb5b337fb1ea31a5ae5a7f2a7c8
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-11-03 00:00:13 +0000

    NULL_tree_ret

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l, height_r;
 
+	if (!tree)
+		return (0);
+
 	height_l = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 	height_r = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 	return (height_r > height_l ? height_r : height_l);
